8101dd8261fddec92e0fb440ac10dac76f5b3445
tim <simiyuutim@gmail.com>
2023-06-19 22:42:41 +0300

    add product details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../app/product-service.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  constructor(private productService: ProductService) { }
+
+  products: any;
+  currentPage: number = 1;
+  pageSize: number = 10;
+  totalItems!: number;
+  totalPages!: number;
+
+  ngOnInit(): void {
+    
+    this.loadPaginatedProducts(this.currentPage);
+  }
+  
+  loadPaginatedProducts(page: number) {
+    this.productService.getPaginatedProducts(page, this.pageSize).subscribe(
+      (data) => {
+        this.products = data.results;
+        this.totalItems = data.count;
+        this.totalPages = Math.ceil(this.totalItems / this.pageSize);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  goToPage(page: number) {
+    if (page >= 1 && page <= this.totalPages) {
+      this.currentPage = page;
+      this.loadPaginatedProducts(this.currentPage);
+    }
+  }
+  
+  
 }
